961ceeaa931b1d5e561a87a95d05ce87089c6782
Christian Donat <c.donat.de@gmail.com>
2024-07-28 16:05:52 +0200

    Initial Commit

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo 'pull'
+git pull
+echo 'install requirements'
+sudo pip3 install -r requirements.txt
+echo 'makemigrations'
+python3 manage.py makemigrations
+echo 'migrate'
+python3 manage.py migrate
+# python3 manage.py collectstatic
+echo 'restart nginx'
+sudo systemctl restart nginx
+echo 'restart gunicorn'
+sudo systemctl restart gunicorn
